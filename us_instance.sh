30861fc0c38136f88d1da6a9827d1a88e855e675
NyeinKoKo <55767055+NyeinKoKo@users.noreply.github.com>
2023-08-31 01:58:40 +0630

    Update us_instance.sh

--- us_instance.sh
+++ us_instance.sh
@@ -21,7 +21,7 @@ plain='\033[0m'
     echo -e "${green}Firewall rule created.${plain}"
   fi
 
-  echo -e "\n${red}GCP UNITED STAGE SERVER${plain}\n"
+  echo -e "\n${red}GCP UNITED STATES SERVER${plain}\n"
 
   # echo -e "Username: ${green}$5${plain}, Password: ${green}$6${plain}, SSH Host :  ${green}$(grep -oP '(?<=EXTERNAL_IP: ).*' <<<"$instance")${plain}"
   # echo -e "SSH Host :  ${green}$(grep -oP '(?<=EXTERNAL_IP: ).*' <<<"$instance")${plain}"
