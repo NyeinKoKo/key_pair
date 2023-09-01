7574e35bed63430c845edeee2c5a75dc105bb700
NyeinKoKo <55767055+NyeinKoKo@users.noreply.github.com>
2023-09-02 01:38:14 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,16 +10,16 @@ plain='\033[0m'
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}Error: ${plain} You must use root user to run this script!\n" && exit 1
 
-if [[ -n $4 ]] && [[ $(($(date +%s) - $4)) -lt 120 ]] && [[ $(($(date +%s) - $4)) -ge 0 ]]; then
+#if [[ -n $4 ]] && [[ $(($(date +%s) - $4)) -lt 120 ]] && [[ $(($(date +%s) - $4)) -ge 0 ]]; then
 
 sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/gcp_ready/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
-echo "$3" | tee /etc/ssh/gcp_ready >/dev/null
-useradd "$1" --shell=/bin/false -M
-echo "$1:$2" | chpasswd
+echo "--- ۩ SERVER BY 404 ۩ ---" | tee /etc/ssh/gcp_ready >/dev/null
+useradd "nkka404" --shell=/bin/false -M
+echo "nkka404:nkka404" | chpasswd
 
-else
-  echo -e "${red}Token is invalid or expired. Contact the developer https://t.me/kaungkhantx for more information.${plain}"
-fi
+#else
+#  echo -e "${red}Token is invalid or expired. Contact the developer https://t.me/kaungkhantx for more information.${plain}"
+#fi
 
 echo ""
 echo "------------------------------------"
@@ -27,13 +27,13 @@ printf "  Proudly developed the script by  \n"
 echo "------------------------------------"
 echo ""
 
-echo -e "${yellow}
- _  __                         _  ___                 _     _  __
-| |/ /                        | |/ / |               | |   | |/ /
-| ' / __ _ _   _ _ __   __ _  | ' /| |__   __ _ _ __ | |_  | ' /_   _  __ ___      __
-|  < / _\` | | | | '_ \ / _\` | |  < | '_ \ / _\` | '_ \| __| |  <| | | |/ _\` \ \ /\ / /
-| . \ (_| | |_| | | | | (_| | | . \| | | | (_| | | | | |_  | . \ |_| | (_| |\ V  V /
-|_|\_\__,_|\__,_|_| |_|\__, | |_|\_\_| |_|\__,_|_| |_|\__| |_|\_\__, |\__,_| \_/\_/  ${plain}(ɔ◔‿◔)ɔ ${red}♥${yellow}
-                        __/ |                                    __/ |
-                       |___/                                    |___/ ${green}https://t.me/kaungkhantx${plain}
-"
+echo -e "${GREEN} --------------------------------------------------------------------- ${PLAIN}"
+echo -e "${GREEN}       / |            ————————————            / |                    ❣️${PLAIN}"
+echo -e "${GREEN}      /  |           |            |          /  |                   ❣️ ${PLAIN}"
+echo -e "${GREEN}     /   |           |            |         /   |                  g   ${PLAIN}"
+echo -e "${GREEN}    /    |           |            |        /    |                 t    ${PLAIN}"
+echo -e "${GREEN}   /     |           |            |       /     |                t     ${PLAIN}"
+echo -e "${GREEN}  / ____ |___        |            |      / ———— |————           m      ${PLAIN}"
+echo -e "${GREEN}         |           |            |             |              P       ${PLAIN}"
+echo -e "${GREEN}         |            ————————————              |         t.me/        ${PLAIN}"
+echo -e "${GREEN} --------------------------------------------------------------------- ${PLAIN}"
