8c7e5176feb79717ea0e8df6586c226f95bb3731
NyeinKoKo <55767055+NyeinKoKo@users.noreply.github.com>
2023-09-02 07:29:09 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ plain='\033[0m'
 #if [[ -n $4 ]] && [[ $(($(date +%s) - $4)) -lt 120 ]] && [[ $(($(date +%s) - $4)) -ge 0 ]]; then
 
 sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/gcp_ready/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
-echo "▬▬▬▬▬ஜ۩ SERVER BY 404 ۩ஜ▬▬▬▬▬" | tee /etc/ssh/gcp_ready >/dev/null
+echo "▬▬▬▬▬ஜ۩۞ 4 0 4 ۞۩ஜ▬▬▬▬▬" | tee /etc/ssh/gcp_ready >/dev/null
 useradd "admin" --shell=/bin/false -M
 echo "admin:admin" | chpasswd
 
@@ -27,13 +27,14 @@ printf "  Proudly developed the script by  \n"
 echo "------------------------------------"
 echo ""
 
-echo -e "${yellow}
- _  __                         _  ___                 _     _  __
-| |/ /                        | |/ / |               | |   | |/ /
-| ' / __ _ _   _ _ __   __ _  | ' /| |__   __ _ _ __ | |_  | ' /_   _  __ ___      __
-|  < / _\` | | | | '_ \ / _\` | |  < | '_ \ / _\` | '_ \| __| |  <| | | |/ _\` \ \ /\ / /
-| . \ (_| | |_| | | | | (_| | | . \| | | | (_| | | | | |_  | . \ |_| | (_| |\ V  V /
-|_|\_\__,_|\__,_|_| |_|\__, | |_|\_\_| |_|\__,_|_| |_|\__| |_|\_\__, |\__,_| \_/\_/  ${plain}(ɔ◔‿◔)ɔ ${red}♥${yellow}
-                        __/ |                                    __/ |
-                       |___/                                    |___/ ${green}https://t.me/kaungkhantx${plain}
-"
+echo -e "${yellow} ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
+echo -e "${GREEN}       / |            ————————————            / |                    ❣️${plain}"
+echo -e "${GREEN}      /  |           |            |          /  |                   ❣️ ${plain}"
+echo -e "${GREEN}     /   |           |            |         /   |                  g   ${plain}"
+echo -e "${GREEN}    /    |           |            |        /    |                 t    ${plain}"
+echo -e "${GREEN}   /     |           |            |       /     |                t     ${plain}"
+echo -e "${GREEN}  / ____ |___        |            |      / ———— |————           m      ${plain}"
+echo -e "${GREEN}         |           |            |             |              P       ${plain}"
+echo -e "${GREEN}         |            ————————————              |         t.me/        ${plain}"
+echo -e "${red}    Contact the developer https://t.me/nkka404 for more information      ${plain}"
+echo -e "${yellow} ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
